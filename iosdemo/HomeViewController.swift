3edaabee860d8ad3d4c892b6e2fdf44f504bc6f5
mbrezov <mariobrezovecki@gmail.com>
2024-04-27 18:10:26 +0200

    button

--- iosdemo/HomeViewController.swift
+++ iosdemo/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  iosdemo
 //
 //  Created by Mario Brezovečki on 27.04.2024..
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
+    
+    private lazy var button: UIButton = {
+        let b = UIButton()
+        b.translatesAutoresizingMaskIntoConstraints = false
+        b.setTitle("button", for: .normal)
+        b.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        b.backgroundColor = .green
+        return b
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.red
+        view.backgroundColor = .white
+        view.addSubview(button)
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
     }
 
+    @objc func buttonTapped() {
+        let vc = LoginViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
 
--- iosdemo/LoginViewController.swift
+++ iosdemo/LoginViewController.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+
+    }
+
+
+}
